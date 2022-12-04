d4bfac57f8b516c627d411ccb3121341eaed5018
Enrique213-VP <colombia00028@gmail.com>
2022-12-03 23:02:49 -0500

    Initialize cloud Firestore, add updates ForgotPassword and Imports in the gradle.

---
+++ app/src/main/java/com/firebaseauth/firestore/FireStoreClass.kt
@@ -0,0 +1,8 @@
+package com.firebaseauth.firestore
+
+import com.google.firebase.firestore.FirebaseFirestore
+
+class FireStoreClass {
+
+    private val mFireStore = FirebaseFirestore.getInstance()
+}
--- app/src/main/java/com/firebaseauth/ui/ForgotPasswordActivity.kt
+++ app/src/main/java/com/firebaseauth/ui/ForgotPasswordActivity.kt
@@ -3,10 +3,12 @@ package com.firebaseauth.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.firebaseauth.R
+import com.firebaseauth.core.BaseActivity
 import com.firebaseauth.databinding.ActivityForgotPasswordBinding
 import com.google.firebase.auth.FirebaseAuth
 
-class ForgotPasswordActivity : AppCompatActivity() {
+class ForgotPasswordActivity : BaseActivity() {
     private lateinit var binding: ActivityForgotPasswordBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,36 +16,47 @@ class ForgotPasswordActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.buttonForgot.setOnClickListener {
+            forgotPassWord()
+        }
+    }
 
-            val email: String = binding.sendEmail.text.toString().trim() { it <= ' ' }
+    private fun forgotPassWord() {
 
-            if (email.isEmpty()) {
-                Toast.makeText(
-                    this@ForgotPasswordActivity,
-                    "Please enter your email",
-                    Toast.LENGTH_LONG
-                ).show()
-            } else {
-                FirebaseAuth.getInstance().sendPasswordResetEmail(email)
-                    .addOnCompleteListener { task ->
-                        if (task.isSuccessful) {
-                            Toast.makeText(
-                                this@ForgotPasswordActivity,
-                                "Email sent successfully to reset your password, please, you must check your email address",
-                                Toast.LENGTH_LONG
-                            ).show()
-                            finish()
-                        } else {
-                            //If the reset is generated a mistake
-                            Toast.makeText(
-                                this@ForgotPasswordActivity,
-                                task.exception!!.message.toString(),
-                                Toast.LENGTH_LONG
-                            ).show()
-                        }
+        //See the progressBar
+        showProgressDialog(resources.getString(R.string.please_wait))
 
+        val email: String = binding.sendEmail.text.toString().trim() { it <= ' ' }
+
+        if (email.isEmpty()) {
+            Toast.makeText(
+                this@ForgotPasswordActivity,
+                "Please enter your email",
+                Toast.LENGTH_LONG
+            ).show()
+        } else {
+            FirebaseAuth.getInstance().sendPasswordResetEmail(email)
+                .addOnCompleteListener { task ->
+
+                    //hide the progressBar
+                    hideProgressDialog()
+
+                    if (task.isSuccessful) {
+                        Toast.makeText(
+                            this@ForgotPasswordActivity,
+                            "Email sent successfully to reset your password, please, you must check your email address",
+                            Toast.LENGTH_LONG
+                        ).show()
+                        finish()
+                    } else {
+                        //If the reset is generated a mistake
+                        Toast.makeText(
+                            this@ForgotPasswordActivity,
+                            task.exception!!.message.toString(),
+                            Toast.LENGTH_LONG
+                        ).show()
                     }
-            }
+
+                }
         }
     }
 }
